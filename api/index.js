b46778f39c9dbdedae6bcf7ed5a491c746c18f0c
debugpoint136 <dpurushotham136@gmail.com>
2015-10-07 14:25:26 -0500

    Added bulk insert capability

--- api/index.js
+++ api/index.js
@@ -36,9 +36,14 @@ var geo2infoSchema = new Schema ({
 	}
 })
 
+var userSchema = new Schema ({
+	name: String,
+	last_modified_date: Date
+})
 
 
 var geo2info = dbConnection.model('GEO2INFO', geo2infoSchema, 'geo2info')
+var User = dbConnection.model('User', userSchema, 'users')
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
@@ -63,6 +68,55 @@ app.post('/posts', function(req, res, next){
 	)})
 	)
 })
+
+function onBulkInsert(err, myDocuments) {
+    if (err) {
+        return next(err);
+    }
+    else {
+        console.log('%userCount users were inserted!', myDocuments.length)
+    }
+}
+
+app.post('/batchinsert', function (req, res, next) {
+/*
+	// This block resets the collection
+	  
+    User.remove({}, function (err) {
+        if (err) {
+            return next(err);
+        }
+    });
+
+    var users = [
+        {"_id": mongoose.Types.ObjectId("54f6612bb0e6af1800b5d33d"), "name": "Andy Sandefer", "__v": 0, "last_modified_date": new Date("2015-03-04T01:28:25.935Z")},
+        {"_id": mongoose.Types.ObjectId("54f6615fb0e6af1800b5d33e"), "name": "Mike Smith", "__v": 0, "last_modified_date": new Date("2015-03-04T01:28:25.935Z")},
+        {"_id": mongoose.Types.ObjectId("54f6616cb0e6af1800b5d33f"), "name": "Bob Jones", "__v": 0, "last_modified_date": new Date("2015-03-04T01:28:25.935Z")},
+        {"_id": mongoose.Types.ObjectId("54f66177b0e6af1800b5d340"), "name": "Aaron Baron", "__v": 0, "last_modified_date": new Date("2015-03-04T01:28:25.935Z")},
+        {"_id": mongoose.Types.ObjectId("54f66182b0e6af1800b5d341"), "name": "Johnny Javascript", "__v": 0, "last_modified_date": new Date("2015-03-04T01:28:25.935Z")},
+        {"_id": mongoose.Types.ObjectId("54f66188b0e6af1800b5d342"), "name": "Paul Pascal", "__v": 0, "last_modified_date": new Date("2015-03-04T01:28:25.935Z")},
+        {"_id": mongoose.Types.ObjectId("54f66190b0e6af1800b5d343"), "name": "Joanna Java", "__v": 0, "last_modified_date": new Date("2015-03-04T01:28:25.935Z")},
+    ];*/
+
+    /***** something like this works perfectly with the POST body *****/
+
+		/*
+		[   
+		{"name": "Deepak Purushotham", "last_modified_date": "2015-03-04T01:28:25.935Z"},
+		{"name": "Mike Jackson", "last_modified_date": "2015-03-04T01:28:25.935Z"},
+		{"name": "Bob Jones", "last_modified_date": "2015-03-04T01:28:25.935Z"},
+		{"name": "Aaron Baron",  "last_modified_date": "2015-03-04T01:28:25.935Z"},
+		{"name": "Johnny Javascript",  "last_modified_date": "2015-03-04T01:28:25.935Z"},
+		{"name": "Paul Pascal",  "last_modified_date": "2015-03-04T01:28:25.935Z"},
+		{"name": "Joanna Java",  "last_modified_date": "2015-03-04T01:28:25.935Z"}
+		]
+		*/
+	
+    User.collection.insert(req.body);
+
+    res.json({'jobStatus': 'MongoDB Refresh Complete - It\'s All Good!'});
+});
+
 
 app.use(errorHandler())
 var server = require('http').createServer(app).listen(3000);
